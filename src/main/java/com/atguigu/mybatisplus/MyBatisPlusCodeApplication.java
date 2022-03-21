05a00df9f56e2c9a8999ab4238626f07b587663a
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-21 23:10:23 +0800

    MybatisPlus_Code 在启动类中使用MapperScan注解进行扫描mapper包中的接口

--- src/main/java/com/atguigu/mybatisplus/MyBatisPlusCodeApplication.java
+++ src/main/java/com/atguigu/mybatisplus/MyBatisPlusCodeApplication.java
@@ -1,9 +1,13 @@
 package com.atguigu.mybatisplus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+// 增加MapperScan("xxx")注解 -> 扫描xx包下面的mapper接口文件
+@MapperScan("com.atguigu.mybatisplus.mapper")
 public class MyBatisPlusCodeApplication {
 
     public static void main(String[] args) {
