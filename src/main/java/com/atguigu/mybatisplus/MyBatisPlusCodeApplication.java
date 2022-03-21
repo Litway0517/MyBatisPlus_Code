c6788b92efb24109a443dac1ea857602f8b9876d
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-21 11:22:09 +0800

    初始化提交

---
+++ src/main/java/com/atguigu/mybatisplus/MyBatisPlusCodeApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisPlusCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisPlusCodeApplication.class, args);
+    }
+
+}
