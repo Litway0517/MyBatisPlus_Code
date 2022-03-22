29236b19955bd455a52fb2838716b7fe37c4c302
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-22 22:46:25 +0800

    MybatisPlus_Code mapper接口增加一个Mapper注解

--- src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import com.atguigu.mybatisplus.pojo.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 // 继承自MyBatisPlus提供的通用mapper接口 BaseMapper. 需要指出改mapper接口操作的数据类型 即User类型
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
 
