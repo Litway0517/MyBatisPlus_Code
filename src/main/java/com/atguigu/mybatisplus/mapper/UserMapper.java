12a1f71b13fc3f7767facee428bf0289d6eff528
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-22 22:47:55 +0800

    MybatisPlus_Code mapper接口增加Repository注解也可以 作为持久层的Bean

--- src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -3,9 +3,12 @@ package com.atguigu.mybatisplus.mapper;
 
 import com.atguigu.mybatisplus.pojo.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 // 继承自MyBatisPlus提供的通用mapper接口 BaseMapper. 需要指出改mapper接口操作的数据类型 即User类型
 @Mapper
+@Repository
 public interface UserMapper extends BaseMapper<User> {
 
 
