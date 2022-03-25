aa6e6184b39f2e550580604311cddc5167b3a375
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-25 21:47:58 +0800

    MybatisPlus_Code 在mapper接口中定义一个方法

--- src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -6,11 +6,20 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 // 继承自MyBatisPlus提供的通用mapper接口 BaseMapper. 需要指出改mapper接口操作的数据类型 即User类型
 @Mapper
 @Repository
 public interface UserMapper extends BaseMapper<User> {
 
+    /**
+     * 得到用户id
+     *
+     * @param id id
+     * @return {@link Map}<{@link String}, {@link Object}>
+     */
+    Map<String, Object> getUserById(Long id);
 
 
 
