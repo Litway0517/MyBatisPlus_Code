70496d26e00c912121d88258f8a0afb6e963e5ee
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-25 20:41:12 +0800

    MybatisPlus_Code 测试BaseMapper中的insert方法

--- src/test/java/com/atguigu/mybatisplus/MyBatisPlusTest.java
+++ src/test/java/com/atguigu/mybatisplus/MyBatisPlusTest.java
@@ -29,4 +29,16 @@ public class MyBatisPlusTest {
         users.forEach(System.out::println);
     }
 
+    @Test
+    public void testInsert() {
+        // SQL -> INSERT INTO user ( id, name, age, email ) VALUES ( ?, ?, ?, ? )
+        User user = new User();
+        user.setName("lit");
+        user.setAge(92);
+        user.setEmail("lit@163.com");
+        int insert = userMapper.insert(user);
+        System.out.println("result: " + insert);
+        System.out.println("打印插入该条数据后得到的主键(这里使用的是雪花算法生成的id, 并不是表中的id): " + user.getId());
+    }
+
 }
