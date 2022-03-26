778d6e774744da64dc44aaa03e4592f95eb2d590
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-26 22:11:42 +0800

    MybatisPlus_Code @Service注解用来标志该类是Service层

--- src/main/java/com/atguigu/mybatisplus/service/impl/UserServiceImpl.java
+++ src/main/java/com/atguigu/mybatisplus/service/impl/UserServiceImpl.java
@@ -4,12 +4,14 @@ import com.atguigu.mybatisplus.mapper.UserMapper;
 import com.atguigu.mybatisplus.pojo.User;
 import com.atguigu.mybatisplus.service.UserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
 /*
     ServiceImpl<T, M>
         - T: 这个泛型是指当前需要操作的mapper接口. 对应的就是UserMapper
         - M: 当前需要操作的实体类的类型. User
  */
+@Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
 
