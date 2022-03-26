8ac1f3ab78fc21ae0e840d384874ed3c2b32e516
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-26 21:42:26 +0800

    MybatisPlus_Code user对应的接口 同样的这个接口继承自MyBatisPlus的通用Service层接口IService

---
+++ src/main/java/com/atguigu/mybatisplus/service/UserService.java
@@ -0,0 +1,7 @@
+package com.atguigu.mybatisplus.service;
+
+import com.atguigu.mybatisplus.pojo.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface UserService extends IService<User> {
+}
