e491734d190fa759a9d200be7c459aadc6872cef
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-28 16:44:54 +0800

    MybatisPlus_Code mybatisplus将批量添加功能放在了通用的IService层而不是通用的Mapper层 因为SQL长度有限制

--- src/test/java/com/atguigu/mybatisplus/service/impl/UserServiceImplTest.java
+++ src/test/java/com/atguigu/mybatisplus/service/impl/UserServiceImplTest.java
@@ -1,9 +1,12 @@
 package com.atguigu.mybatisplus.service.impl;
 
+import com.atguigu.mybatisplus.pojo.User;
 import com.atguigu.mybatisplus.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 
 /**
@@ -17,6 +20,21 @@ public class UserServiceImplTest {
 
     @Autowired
     private UserService userService;
+
+    // 批量添加
+    @Test
+    public void testBatchInsert() {
+        ArrayList<User> userArrayList = new ArrayList<User>();
+        for (int i = 0; i <= 10; i++) {
+            User user = new User();
+            user.setName("lit" + i);
+            user.setAge(20 + i);
+            user.setEmail("lit" + i + "@163.com");
+            userArrayList.add(user);
+        }
+        boolean b = userService.saveBatch(userArrayList);
+        System.out.println("批量添加 -> " + b);
+    }
 
     @Test
     public void testGetCount() {
