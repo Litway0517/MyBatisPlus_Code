c537060fc98278b77e2e1373a7328e048df647d7
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-29 16:26:20 +0800

    MybatisPlus_Code QueryWrapper执行一个方法之后返回的仍是QueryWrapper 所以能进行链式编程

--- src/test/java/com/atguigu/mybatisplus/MyBatisPlusWrapperTest.java
+++ src/test/java/com/atguigu/mybatisplus/MyBatisPlusWrapperTest.java
@@ -21,6 +21,7 @@ public class MyBatisPlusWrapperTest {
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<User>();
         /*
             设置条件 -> 查询用户名包含a, 年龄在20-30之间, 邮箱信息不为null的用户信息
+            链式编程, 因为每一个条件返回的都是QueryWrapper, 所以还是能够调用其他方法设置条件
             - column: 是数据库表的字段名称, 而不是实体类的属性名
          */
         userQueryWrapper
