8e6b16b808853e6404c32e1f9a826aa1a55db637
Mingw0717-Dell <xmw0517@gmail.com>
2022-03-31 21:48:38 +0800

    MybatisPlus_Code 测试插件

---
+++ src/test/java/com/atguigu/mybatisplus/MyBatisPlusConfigTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyBatisPlusConfigTest {
+
+    @Test
+    void myBatisPlusInterceptor() {
+    }
+}
