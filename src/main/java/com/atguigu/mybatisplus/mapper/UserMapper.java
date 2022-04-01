29367a45432794f212d04a5e30198cc26e432c74
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-01 15:52:51 +0800

    MybatisPlus_Code 编写自定义的接口 根据年龄进行查询用户信息 要求实现分页功能(此时是借助MybatisPlus的分页插件实现的)

--- src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -3,7 +3,10 @@ package com.atguigu.mybatisplus.mapper;
 
 import com.atguigu.mybatisplus.pojo.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
@@ -20,6 +23,16 @@ public interface UserMapper extends BaseMapper<User> {
      * @return {@link Map}<{@link String}, {@link Object}>
      */
     Map<String, Object> getUserById(Long id);
+
+    /**
+     * 通过年龄查询用户信息并分页! 主要是分页功能结合自定义的SQL
+     * 如若想在自定义SQL中实现分页功能, 那么mapper的这个方法的接口的第一个参数必须是Page. 返回值为Page<User>
+     *
+     * @param page 页面. 第一个参数必须是Page对象, 这样才能借用MyBatisPlus框架的分页功能
+     * @param age  年龄
+     * @return {@link Page}<{@link User}> 返回值的类型也是Page<User> 泛型为实体类对象
+     */
+    Page<User> selectPageVo(@Param("page") Page<User> page, @Param("age") Integer age);
 
 
 
