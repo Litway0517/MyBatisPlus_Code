2697beb0527f1c2e1c40cdda79473c8b19f7df5e
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-01 17:55:59 +0800

    MybatisPlus_Code 在MybatisPlus核心配置类中增加 乐观锁 插件(其本质也是一个拦截器)

--- src/main/java/com/atguigu/mybatisplus/config/MyBatisPlusConfig.java
+++ src/main/java/com/atguigu/mybatisplus/config/MyBatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.atguigu.mybatisplus.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +23,10 @@ public class MyBatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor myBatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        // 添加分页插件
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 添加乐观锁插件
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return mybatisPlusInterceptor;
     }
 
