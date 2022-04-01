ee5f492a723a43635755ff25c23b793802a2504b
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-01 16:21:31 +0800

    MybatisPlus_Code 编写自定义mapper接口时第一个参数必须为Page对象 返回值也必须是Page<泛型> 这样就能够借助MybatisPlus实现分页

--- src/test/java/com/atguigu/mybatisplus/MyBatisPlusConfigTest.java
+++ src/test/java/com/atguigu/mybatisplus/MyBatisPlusConfigTest.java
@@ -21,6 +21,27 @@ class MyBatisPlusConfigTest {
     @Autowired
     private UserMapper userMapper;
 
+    // 自定义的SQL语句 搭配 MybatisPlus的分页插件
+    @Test
+    public void testSelectPageVo() {
+        /*
+            SQL -> select uid, user_name, age, email, is_deleted from `t_user` where is_deleted = '0'
+                    and age > ? LIMIT ?,?
+
+         */
+        // long current参数是当前页, size是每页中记录的数目
+        Page<User> userPage = new Page<User>(2, 3);
+        // 传入参数
+        Page<User> pageVo = userMapper.selectPageVo(userPage, 20);
+        System.out.println("总页数 -> " + pageVo.getPages());
+        System.out.println("总记录数 -> " + pageVo.getTotal());
+        System.out.println("是否有下一页 -> " + pageVo.hasNext());
+        System.out.println("当前页记录 -> " + pageVo.getRecords());
+        System.out.println(pageVo);
+    }
+
+
+    // MybatisPlus的分页功能 搭配 LambdaQueryWrapper查询器
     @Test
     public void testSelectByPage() {
         /*
@@ -28,12 +49,12 @@ class MyBatisPlusConfigTest {
             分页的参数是: 该页索引, 每页中的条数. 从index处开始的后面一页
                 比如一页3条, 那么第三页的参数为 8, 3
          */
-        Page<User> userPage = new Page<User>(2, 4);
+        Page<User> userPage = new Page<User>(2, 3);
         // 设置条件
         LambdaQueryWrapper<User> userLambdaQueryWrapper = new LambdaQueryWrapper<User>();
         userLambdaQueryWrapper.like(User::getName, "admin");
 
-        // 数据都放在了page里面
+        // 数据都放在了page里面 本次查询不使用条件
         Page<User> page = userMapper.selectPage(userPage, null);
         System.out.println("总页数 -> " + page.getPages());
         System.out.println("总记录数 -> " + page.getTotal());
