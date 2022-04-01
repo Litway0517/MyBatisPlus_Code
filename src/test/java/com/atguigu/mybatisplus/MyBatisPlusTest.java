db83c50ac6c3627123c8a1d6370edc73a3084f02
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-01 12:13:11 +0800

    MybatisPlus_Code

--- src/test/java/com/atguigu/mybatisplus/MyBatisPlusTest.java
+++ src/test/java/com/atguigu/mybatisplus/MyBatisPlusTest.java
@@ -3,7 +3,6 @@ package com.atguigu.mybatisplus;
 
 import com.atguigu.mybatisplus.mapper.UserMapper;
 import com.atguigu.mybatisplus.pojo.User;
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
