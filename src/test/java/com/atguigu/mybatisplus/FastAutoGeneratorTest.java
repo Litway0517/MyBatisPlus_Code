4ad8057c3b47f3651175fdb4d8b076266ed366bd
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-02 12:24:02 +0800

    MybatisPlus_Code 代码生成 就是一个过程 需要创建一个类将官网给出的方法执行以下 放在PSVM中运行

--- src/test/java/com/atguigu/mybatisplus/FastAutoGeneratorTest.java
+++ src/test/java/com/atguigu/mybatisplus/FastAutoGeneratorTest.java
@@ -13,7 +13,7 @@ public class FastAutoGeneratorTest {
                 "jdbc:mysql://localhost:3306/mybatisplus_code?characterEncoding=utf-8&userSSL=false",
                         "root", "root")
                 .globalConfig(builder -> {
-                    builder.author("DELL_")          // 设置作者
+                    builder.author("DELL_")             // 设置作者
                             .enableSwagger()            // 开启 swagger 模式
                             .fileOverride()             // 覆盖已生成文件
                             .outputDir("F://IDEA-Java//notes//11-SGG-MyBatisPlus//AutoGenerator"); // 指定输出目录
@@ -21,7 +21,7 @@ public class FastAutoGeneratorTest {
                 .packageConfig(builder -> {
                     builder.parent("com.atguigu")           // 设置父包名
                             .moduleName("mybatisplus")      // 设置父包模块名 -> 组成结果com.atguigu.mybatisplus
-                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "F://IDEA-Java//notes//11-SGG-MyBatisPlus//AutoGenerator//mapper")); // 设置mapperXml生成路径
+                            .pathInfo(Collections.singletonMap(OutputFile.mapper, "F://IDEA-Java//notes//11-SGG-MyBatisPlus//AutoGenerator//mapper")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
                     builder.addInclude("t_user")            // 设置需要生成的表名
