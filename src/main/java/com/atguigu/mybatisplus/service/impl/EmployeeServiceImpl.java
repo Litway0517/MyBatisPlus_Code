c5d2b7a2e3d7d0de4ee0f69e5d4d3ae8f1ae4bbd
Litway_ <xmw0517@gmail.com>
2023-05-07 20:51:45 +0800

    MybatisPlus_Code  Service层测试 注意接口上不能使用@Service注解

--- src/main/java/com/atguigu/mybatisplus/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/atguigu/mybatisplus/service/impl/EmployeeServiceImpl.java
@@ -4,10 +4,12 @@ import com.atguigu.mybatisplus.mapper.EmployeeMapper;
 import com.atguigu.mybatisplus.pojo.Employee;
 import com.atguigu.mybatisplus.service.EmployeeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Litway
  * @version 1.0
  */
+@Service
 public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
 }
--- src/test/java/com/atguigu/mybatisplus/service/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/atguigu/mybatisplus/service/impl/EmployeeServiceImplTest.java
@@ -1,6 +1,12 @@
 package com.atguigu.mybatisplus.service.impl;
 
+import com.atguigu.mybatisplus.pojo.Employee;
+import com.atguigu.mybatisplus.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 
 /**
@@ -9,5 +15,24 @@ import org.springframework.boot.test.context.SpringBootTest;
  */
 @SpringBootTest
 public class EmployeeServiceImplTest {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Test
+    public void testSave() {
+        Employee employee = new Employee();
+        employee.setEmployeeId(207);
+        employee.setFirstName("Test");
+        employee.setLastName("John1");
+        employee.setEmail("FHIGHA");
+        employee.setPhoneNumber("515.123.8181");
+        employee.setHireDate(new Date());
+        employee.setJobId("AC_ACCOUNT");
+        employee.setSalary(8300.00);
+        employee.setManagerId(205);
+        employee.setDepartmentId(110);
+        employeeService.save(employee);
+    }
 
 }
